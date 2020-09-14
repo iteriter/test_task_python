183988c3f1669b9b77291e3f5741ee3813755a23
Mark R <theiterance@gmail.com>
2020-09-14 12:19:54 +0300

    Add tests placeholders

---
+++ tests/tests.py
@@ -0,0 +1,29 @@
+def test_get_all_posts():
+    pass
+
+def test_get_all_posts_sorted():
+    pass
+
+def test_get_all_posts_comments_count():
+    pass
+
+def test_get_all_posts_only_valid():
+    pass
+
+def test_get_post():
+    pass
+
+def test_get_post_comments_sorted():
+    pass
+
+def test_get_post_comments_count():
+    pass
+
+def test_get_post_deleted():
+    pass
+
+def test_get_post_non_existent():
+    pass
+
+def test_get_post_future():
+    pass
