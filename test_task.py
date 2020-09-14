4c4a1dba522c24e14bfb3fd35c64d4f3cefb8833
M <singularity@singularity.local>
2020-09-14 11:44:04 +0300

    Add boilerplate code

---
+++ test_task.py
@@ -0,0 +1,10 @@
+import asyncio
+import aiohttp
+
+if __name__ == "__main__":
+    pass
+    # start aiohttp server
+    # listen for get requests
+    # handle get requests with asyncio
+        # handle all posts request
+        # handle single post request
