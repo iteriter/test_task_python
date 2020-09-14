a3e81c4ca8f76600dd0f3fb32ba744b4221232f8
Mark R <theiterance@gmail.com>
2020-09-14 14:37:25 +0300

    Implement tests

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from aiohttp import web
 from test_task import PostsApi
 
@@ -13,33 +14,86 @@ def server(loop, aiohttp_client):
 
     return loop.run_until_complete(aiohttp_client(app))
 
+def get_item_ids(items):
+    return [item['id'] for item in items]
+
 async def test_get_all_posts(server):
     resp = await server.get('/')
+    content = await resp.text()
+    data = json.loads(content)
+
+    assert resp.status == 200
+    assert data['posts_count'] == 4
+
+    # check number of posts returned is correct
+    assert len(data['posts']) == 4
+
+async def test_get_all_posts_sorted(server):
+    resp = await server.get('/')
+    content = await resp.text()
+    data = json.loads(content)
+
+    # check posts returned in correct orders
+    ids = get_item_ids(data['posts'])
+    assert ids == (5, 1, 2, 8) or ids == (5, 2, 1, 8)
+
+async def test_get_all_posts_comments_count(server):
+    resp = await server.get('/')
+    content = await resp.text()
+    data = json.loads(content)
+
+    for post in data['posts']:
+        if post['id'] == 1:
+            assert post['comments_count'] == 2
+
+async def test_get_all_posts_only_valid(server):
+    resp = await server.get('/')
+    content = await resp.text()
+    data = json.loads(content)
+
+    ids = get_item_ids(data['posts'])
+    assert 6 not in ids
+    assert 7 not in ids
+
+async def test_get_post(server):
+    resp = await server.get('/post/1')
+
     assert resp.status == 200
 
-def test_get_all_posts_sorted():
-    pass
+async def test_get_post_comments_count(server):
+    resp = await server.get('/post/1')
+    content = await resp.text()
+    data = json.loads(content)
 
-def test_get_all_posts_comments_count():
-    pass
+    assert data['comments_count'] == 2
 
-def test_get_all_posts_only_valid():
-    pass
+    resp = await server.get('/post/2')
+    content = await resp.text()
+    data = json.loads(content)
 
-def test_get_post():
-    pass
+    assert data['comments_count'] == 0
 
-def test_get_post_comments_sorted():
-    pass
+async def test_get_post_comments_sorted(server):
+    resp = await server.get('/post/1')
+    content = await resp.text()
+    data = json.loads(content)
 
-def test_get_post_comments_count():
-    pass
+    comments = data['comments']
+    ids = get_item_ids(comments)
 
-def test_get_post_deleted():
-    pass
+    assert ids == (2,1)
 
-def test_get_post_non_existent():
-    pass
+async def test_get_post_deleted(server):
+    resp = await server.get('/post/6')
 
-def test_get_post_future():
-    pass
+    assert resp.status == 404
+
+async def test_get_post_non_existent(server):
+    resp = await server.get('/post/100')
+
+    assert resp.status == 404
+
+async def test_get_post_future(server):
+    resp = await server.get('/post/7')
+
+    assert resp.status == 404
